7c8961befd0a3f805e556d4bbabd3f53e9417dea
Arda Özgür <ardaozgurtr@outlook.com>
2025-07-02 13:25:19 +0300

    Cart total feature added

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, 'cart_summary.html', {'cart_products':cart_products, 'quantities':quantities, "totals":totals})
 
 def cart_add(request):
     # Get the cart
@@ -13,12 +18,13 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # Get stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
 
         # Look up product in DataBase
         product = get_object_or_404(Product, id=product_id)
 
         # Save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
         # Get cart quantity
         cart_quantity = cart.__len__()
@@ -31,8 +37,25 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        # Call delete function in cart
+        cart.delete(product=product_id)
+        response = JsonResponse({'product':product_id})
+        return response
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty':product_qty})
+        return response
+        #return redirect('cart_summary')
 
