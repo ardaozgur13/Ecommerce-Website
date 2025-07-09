9b322de145c06dbf13aa0724445cde2ea208570a
Arda Özgür <ardaozgurtr@outlook.com>
2025-07-09 12:19:03 +0300

    Billing Info added

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,15 +1,96 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
+import json
+from cart.cart import Cart
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
+def search(request):
+    # Determine if they fill out the form
+    if request.method == "POST":
+        searched = request.POST['searched']
+        # Query The Products DB Model
+        searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+        if not searched:
+            messages.success(request, "That product is not exist, please try again")
+            return render(request, 'search.html', {})
+        else:
+            return render(request, 'search.html', {'searched':searched})
+    else:
+        return render(request, 'search.html', {})
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+
+        if form.is_valid() or shipping_form.is_valid():
+            form.save()
+            shipping_form.save()
+            messages.success(request, ("Your Info Has Been Updated..."))
+            return redirect('home')
+        return render(request, 'update_info.html', {'form':form, 'shipping_form':shipping_form})
+    else:
+        messages.success(request, ("You Must Be Log In to Access That Page..."))
+        return redirect('home')
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Did they fill out the form
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            # Is the form valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your Password Has Been Updated...")
+                login(request, current_user)
+                return redirect('home')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, 'update_password.html', {'form':form})
+    else:
+        messages.success(request, ("You Must Be Log In to Access That Page..."))
+        return redirect('home')
+    
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, ("User Has Been Updated..."))
+            return redirect('home')
+        return render(request, 'update_user.html', {'user_form':user_form})
+    else:
+        messages.success(request, ("You Must Be Log In to Access That Page..."))
+        return redirect('home')
+
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, 'category_summary.html', {'categories':categories})
+
 
 def category(request,foo):
     # Replace Hyphens with Spaces
@@ -38,6 +119,22 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+
+            # Do some shopping cart stuff
+            current_user = Profile.objects.get(user__id=request.user.id)
+            # Get there save cart from database
+            saved_cart = current_user.old_cart
+            # Convert db string to python dictionary
+            if saved_cart:
+                # Convert Dictionary using JSON
+                converted_cart = json.loads(saved_cart)
+                # Add the loaded cart dictionary to our session
+                cart = Cart(request)
+                # Loop thru the cart and add the items from db
+                for key,value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
+
+
             messages.success(request, ("You have been logged in..."))
             return redirect('home')
         else:
@@ -62,8 +159,8 @@ def register_user(request):
             #login user
             user = authenticate(request, username=username, password=password)
             login(request, user)
-            messages.success(request, ("You have been registered successfully..."))
-            return redirect('home')
+            messages.success(request, ("You have been registered successfully, please fill out your user info..."))
+            return redirect('update_info')
         else:
             messages.success(request, ("There was an error, please try again..."))
             return redirect('register')
