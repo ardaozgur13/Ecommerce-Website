137359a3eafb16b05eb7f9d061aafd79c64e20a3
Arda Özgür <ardaozgurtr@outlook.com>
2025-07-11 21:46:04 +0300

    deployin trying

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-qn(q6ay5l@25^8a9q2eiguizf$ph#*jb0ds51d#n7&3w$zva6$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-website-production-ada7.up.railway.app', 'https://ecommerce-website-production-ada7.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','ecommerce-website-production-ada7.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://ecommerce-website-production-ada7.up.railway.app']
 
 
